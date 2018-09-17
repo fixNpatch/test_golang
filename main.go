45191be950f142d325a3306288b5629bdedae555
fixN <seo@gm.com>
2018-09-17 15:19:18 +0400

    init

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 )
 
 func main() {
-	b, err := ioutil.ReadFile("main/file.txt") // just pass the file name
+	b, err := ioutil.ReadFile("file.json") // just pass the file name
 	if err != nil {
 		fmt.Print(err)
 	}
-
-	fmt.Println(hex.Dump(b))
-
-	fmt.Println(b) // print the content as 'bytes'
 
 	str := string(b) // convert content to a 'string'
 
