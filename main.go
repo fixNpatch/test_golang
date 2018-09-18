6b1c236ef2195046847e556d4af1dcd609643b6a
fixN <seo@gm.com>
2018-09-18 11:16:15 +0400

    init

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
-func main() {
-	b, err := ioutil.ReadFile("file.json") // just pass the file name
-	if err != nil {
-		fmt.Print(err)
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
+}
 
-	str := string(b) // convert content to a 'string'
+func main() {
+	file, err := os.Open("file.txt")
+	check(err)
+	defer file.Close()
+
+	bear := make([]byte, 5)
+	str, err := file.Read(bear) // convert content to a 'string'
+	check(err)
 
 	fmt.Println(str) // print the content as a 'string'
 }
